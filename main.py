c43e7a24eab776fbefdd620832e3103909d22937
laurab04-design <laura_b_04@hotmail.com>
2025-05-18 19:15:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import playwright.__main__ as pwmain
+pwmain.main(["install", "chromium"])
 from fastapi import FastAPI
 from golden_critiques import run_scraper
 from drive_utils import upload_to_drive
